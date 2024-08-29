e84cc82ec115ae0f61f891b6cc5cb5b03fa57a5e
jjuya <404_jjuya@kakao.com>
2024-08-29 18:23:43 +0900

    feat(content) : add translate

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,28 +1,30 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import LocaleSwitcher from '@/components/LocaleSwitcher'
+import { useTranslations } from 'next-intl'
 
 export const HomeView = ({ ...props }) => {
 	const router = useRouter()
 	const { locale, locales, defaultLocale } = router
 
+	const headerMessage = useTranslations('Header')
+	const footerMessage = useTranslations('Footer')
+
 	return (
 		<div>
-			<h1>Index page</h1>
+			<h1>{headerMessage('title')}</h1>
+			<h2>{headerMessage('subTitle')}</h2>
+
 			<p>Current locale: {locale}</p>
 			<p>Default locale: {defaultLocale}</p>
 			<p>Configured locales: {JSON.stringify(locales)}</p>
 
-			<LocaleSwitcher />
+			<LocaleSwitcher/>
 
-			<Link href="/gsp">To getStaticProps page</Link>
-			<br />
 
-			<Link href="/gsp/first">To dynamic getStaticProps page</Link>
-			<br />
-
-			<Link href="/gssp">To getServerSideProps page</Link>
-			<br />
+			<h2>
+				{footerMessage('title')}
+			</h2>
 		</div>
 	)
 }
